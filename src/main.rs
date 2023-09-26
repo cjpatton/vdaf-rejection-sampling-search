fd0435456e563eb85d1f88e23255da8474297c5d
David Cook <dcook@divviup.org>
2023-09-26 15:33:18 -0500

    Default to using all available CPU cores

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::num::NonZeroUsize;
+
 use clap::{value_parser, Arg, Command};
 use prio::field::Field64;
 
@@ -5,7 +7,15 @@ use vdaf_rejection_sampling_search::{search, Config};
 
 fn main() {
     let matches = app().get_matches();
-    let jobs = *matches.get_one("jobs").unwrap();
+    let jobs = matches
+        .get_one("jobs")
+        .copied()
+        .or_else(|| {
+            std::thread::available_parallelism()
+                .ok()
+                .map(NonZeroUsize::get)
+        })
+        .unwrap_or(1);
     let prg_iterations = *matches.get_one("prg-iterations").unwrap();
     let config = Config::new(jobs, prg_iterations);
     search::<Field64>(config);
@@ -17,8 +27,7 @@ fn app() -> Command {
             Arg::new("jobs")
                 .short('j')
                 .long("jobs")
-                .value_parser(value_parser!(usize))
-                .default_value("1"),
+                .value_parser(value_parser!(usize)),
         )
         .arg(
             Arg::new("prg-iterations")
